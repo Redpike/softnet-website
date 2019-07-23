d5eb87d53471f557c6263d726691be96fc43f4db
Redpike <rsokulski@op.pl>
2019-07-23 21:22:08 +0200

    Install plugin for SASS support

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -13,7 +13,12 @@ const GlobalStyle = createGlobalStyle`
     
     body {
         margin: 0;
+        font-family: 'Montserrat Regular', sans-serif;
         font-size: 1.6rem;
+    }
+
+    h1, h2, h3, h4, h5 {
+        font-family: 'DarkerGrotesque Bold', sans-serif;
     }
 `;
 
