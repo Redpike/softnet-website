9162d10e81923568afdf0fc1269486aecdab19d6
Redpike <rsokulski@op.pl>
2019-09-30 22:04:09 +0200

    Make CompanyGoals component more responsive

--- src/components/CompanyGoals/CompanyGoals.js
+++ src/components/CompanyGoals/CompanyGoals.js
@@ -6,6 +6,22 @@ const GoalItem = styled.div`
     flex: 1 0 30%;
     margin: 5px;
     text-align: center;
+
+    > div {
+        margin: 30px 0;
+    }
+
+    > div:first-child {
+        margin: 0 0 30px;
+    }
+
+    > div:last-child {
+        margin: 30px 0 0;
+    }
+`;
+
+const GoalItemIcon = styled.figure`
+
 `;
 
 const GoalItemTitle = styled.div`
@@ -23,12 +39,19 @@ const GoalItemsWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+
+    ${({ theme }) => theme.mq.desktop_lt} {
+        flex-direction: column;
+    }
 `;
 
 const CompanyGoals = () => (
     <GoalItemsWrapper>
         {companyGoalItems.map(element => (
             <GoalItem key={element.id}>
+                {/* <GoalItemIcon>
+                    
+                </GoalItemIcon> */}
                 <GoalItemTitle>
                     {element.title}
                 </GoalItemTitle>
